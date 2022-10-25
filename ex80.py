a3ac5d333fb18896d5b5695062e08a9b62faa94d
Reni Brito <114774905+ReniBrito@users.noreply.github.com>
2022-10-25 14:47:52 -0300

    Update ex80.py

--- ex80.py
+++ ex80.py
@@ -1,9 +1,19 @@
-valores = []
-for  indices in range(0, 5):
-    v = (int(input('Digite o valor:')))
-    if indices == 0:
-        valores.append(v)
-    elif v > len(valores)-1:
-        valores.append(v)
+lista = []
 
-print(valores)
+cont = 0
+while True:
+    n = int(input('Digite um valor: '))
+    if n not in lista:
+        cont += 1
+        lista.append(n)
+
+
+    resp = str(input('Deseja continuar? [S/N]')).strip().upper()[0]
+    if resp == 'N':
+        break
+print(f'Sua lista foi com esses valores {lista}')
+print(f'vc digitou {len(lista)} valores')
+print(f'Sua lista crecente é {sorted(lista)}')
+print(f'sua lista decrecente é {sorted(lista, reverse =True)}')
+print('O valor 5 esta na lista' if 5 in lista else 'O valor 5 não esta na lista')
+    
